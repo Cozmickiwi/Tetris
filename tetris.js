d434f77049608de2f2871140eb91c6c0696e84d7
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-08 20:48:38 +1300

    add square

--- tetris.js
+++ tetris.js
@@ -11,9 +11,14 @@ function gameGrid(){
 
 gameGrid();
 
+
+
 function game(){
     let line = [5, 15, 25, 35];
+    let square = [5, 6, 15, 16];
+    let shape = [5, 15, 25, 35];
     let takenTiles = [];
+    let takenTilesCopy = [];
     let shapeHis = [];
     let currentShape = 'line';
     let shapeSettled = false;
@@ -24,42 +29,43 @@ function game(){
     let repaint = false;
     let ignoreRow;
     let dropCount = 0;
+    let cont = true;
     window.addEventListener('keydown', event =>{
         function horizMove() {
-            let prevMove = line.slice();
+            let prevMove = shape.slice();
             if(event.key == 'ArrowRight'){
-                for(let u = 0; u<line.length; u++){
-                    if(line[u] % 10 == 0){
+                for(let u = 0; u<shape.length; u++){
+                    if(shape[u] % 10 == 0){
                         return;
                     }
-                    else if(takenTiles.includes(line[u]+1)){
+                    else if(takenTiles.includes(shape[u]+1)){
                         return;
                     }
                 }
-                for(let q = 0; q<line.length; q++){
-                    document.getElementById(line[q]).style.backgroundColor = 'rgb(90, 90, 240)'
+                for(let q = 0; q<shape.length; q++){
+                    document.getElementById(shape[q]).style.backgroundColor = 'rgb(90, 90, 240)'
                 }
-                for(let g = 0; g<line.length; g++){
-                    line[g] += 1;
+                for(let g = 0; g<shape.length; g++){
+                    shape[g] += 1;
                     
-                    document.getElementById(line[g]).style.backgroundColor = 'red';
+                    document.getElementById(shape[g]).style.backgroundColor = 'red';
                 }
             }
             else if(event.key == 'ArrowLeft'){
-                for(let u = 0; u<line.length; u++){
-                    if((line[u]-1) % 10 == 0){
+                for(let u = 0; u<shape.length; u++){
+                    if((shape[u]-1) % 10 == 0){
                         return;
                     }
-                    else if(takenTiles.includes(line[u]-1)){
+                    else if(takenTiles.includes(shape[u]-1)){
                         return;
                     }
                 }
-                for(let q = 0; q<line.length; q++){
-                    document.getElementById(line[q]).style.backgroundColor = 'rgb(90, 90, 240)'
+                for(let q = 0; q<shape.length; q++){
+                    document.getElementById(shape[q]).style.backgroundColor = 'rgb(90, 90, 240)'
                 }
-                for(let g = 0; g<line.length; g++){
-                    line[g] -= 1;
-                    document.getElementById(line[g]).style.backgroundColor = 'red';
+                for(let g = 0; g<shape.length; g++){
+                    shape[g] -= 1;
+                    document.getElementById(shape[g]).style.backgroundColor = 'red';
                 }
             }
             else if(event.key == 'ArrowDown' && downEventFired == false){
@@ -78,97 +84,109 @@ function game(){
                             shapeMove();
                         }, time);
                     }
-                })
+                });
+            }
+            else if(event.code == 'Space'){
+                debugger;
             }
         }
         horizMove();
-    })
+    });
     interval = setInterval(() =>{
         shapeMove();
     }, time);
     function shapeMove () {
         i++
         let frame = i;
+        
+        
+        
         if(currentShape == 'line'){
+            
             if(frame < 5){
                 for(let x=0;x<frame;x++){
-                        document.getElementById(line[x]).style.backgroundColor = 'red';
-                    }
+                    document.getElementById(shape[x]).style.backgroundColor = 'red';
+                }
             }
-            else{
-                //clearInterval(interval);
-                for(let k=0; k<4; k++){
-                    document.getElementById(line[k]).style.backgroundColor = 'rgb(90, 90, 240)'
+        }
+        else if(currentShape == 'square'){
+            
+            if(frame < 3){
+                for(let x=0;x<frame;x++){
+                    document.getElementById(shape[x]).style.backgroundColor = 'red';
                 }
-                shapeHis = line.slice();
-                let settled = false;
-                for(let j=0; j<4;j++){
-                    line[j]+=10;
-                    if(line[j] > 200){
-                        settled = true;
-                    }
-                    else if(takenTiles.includes(line[j])){
-                        settled = true;
-                    }
-                    if(settled == true){
-                    for(let n = 0; n<line.length;n++){
-                        if(line[n] <= 200 && !(takenTiles.includes(line[n]))){
-                            document.getElementById(line[n]).style.backgroundColor = 'rgb(90, 90, 240)';
-                        }
-                        document.getElementById(shapeHis[n]).style.backgroundColor = 'red';
-                    }
-                    for(let l = 0; l<4; l++){
-                        takenTiles.push(shapeHis[l]);
-                    }
-                    takenTiles = MergeSort(takenTiles);
-                    //for(let q = 10; q<=200; q+=10){
-                        let n = (takenTiles.length)-1;
-                        if(n>=9){
-                            for(let a = 0; a<=n;a++){
-                                if(a>=9 && (takenTiles[a] % 10) == 0 && takenTiles[a-9] == (takenTiles[a] - 9)){
-                                    repaint = true;
-                                    ignoreRow = takenTiles[a];
-                                    takenTiles.splice((a-9), 10)
-                                    n = (takenTiles.length)-1;
-                                    a-=10;
-                                    dropCount+=10;
-                                }
-                            }
-                            if(repaint == true){
-                                repaint = false;
-                                for(let r = 1; r<=200; r++){
-                                    document.getElementById(r).style.backgroundColor = 'rgb(90, 90, 240)';
-                                }
-                                for(let f = 0; f<takenTiles.length; f++){
-                                        if(takenTiles[f] <= (ignoreRow+1)){
-                                            takenTiles[f] += dropCount;
-                                        }
-                                    }
-                                    for(let w = 0; w<takenTiles.length; w++){
-                                        document.getElementById(takenTiles[w]).style.backgroundColor = 'red';
-                                    }
-                            }
-                        }
-                    //}
-                    console.log(takenTiles);
-                    line = [5, 15, 25, 35];
-                    dropCount = 0;
-                    ignoreRow = undefined;
-                    i = 0;
-                    settled = false;
-                    return;
-                }
-                    document.getElementById(line[j]).style.backgroundColor = 'red';
-                }
-                
-                /*for(let k=0; k<4;k++){
-                    for(let y=0; y<4)
-                    if(shapeHis[k] != line[k]){
-                        document.getElementById(shapeHis[k]).style.backgroundColor = 'rgb(90, 90, 240)';
-                    }
-                }
-                */
             }
+        }
+        for(let k=0; k<shape.length; k++){
+            document.getElementById(shape[k]).style.backgroundColor = 'rgb(90, 90, 240)'
+        }
+        shapeHis = shape.slice();
+        let settled = false;
+        for(let j=0; j<shape.length;j++){
+            shape[j]+=10;
+            if(shape[j] > 200){
+                settled = true;
+            }
+            else if(takenTiles.includes(shape[j])){
+                settled = true;
+            }
+            if(settled == true){
+            for(let n = 0; n<shape.length;n++){
+                if(shape[n] <= 200 && !(takenTiles.includes(shape[n]))){
+                    document.getElementById(shape[n]).style.backgroundColor = 'rgb(90, 90, 240)';
+                }
+                document.getElementById(shapeHis[n]).style.backgroundColor = 'red';
+            }
+            for(let l = 0; l<4; l++){
+                takenTiles.push(shapeHis[l]);
+            }
+            takenTiles = MergeSort(takenTiles);
+            let n = (takenTiles.length)-1;
+            if(n>=9){
+                takenTilesCopy = takenTiles.slice();
+                for(let a = 0; a<=n;a++){
+                    if(a>=9 && (takenTiles[a] % 10) == 0 && takenTiles[a-9] == (takenTiles[a] - 9)){
+                        repaint = true;
+                        ignoreRow = takenTiles[a];
+                        takenTiles.splice((a-9), 10)
+                        n = (takenTiles.length)-1;
+                        a-=10;
+                        dropCount+=10;
+                    }
+                }
+                if(repaint == true){
+                    repaint = false;
+                    for(let r = 1; r<=200; r++){
+                        document.getElementById(r).style.backgroundColor = 'rgb(90, 90, 240)';
+                    }
+                    for(let f = 0; f<takenTiles.length; f++){
+                        if(takenTiles[f] <= (ignoreRow+1)){
+                            takenTiles[f] += dropCount;
+                        }
+                    }
+                    for(let w = 0; w<takenTiles.length; w++){
+                        document.getElementById(takenTiles[w]).style.backgroundColor = 'red';
+                    }
+                }
+            }
+            console.log(takenTiles);
+            //line = [5, 15, 25, 35];
+            let ranShape = Math.floor(Math.random() * 2);
+            if(ranShape == 0){
+                currentShape = 'line';
+                shape = line.slice();
+            }
+            else if(ranShape == 1){
+                currentShape = 'square';
+                shape = square.slice();
+            }
+            dropCount = 0;
+            ignoreRow = undefined;
+            i = 0;
+            settled = false;
+            return;
+        }
+            document.getElementById(shape[j]).style.backgroundColor = 'red';
         }
     }
 }
