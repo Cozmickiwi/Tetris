563351a3a2416aa0d47404d21ee626bda4445c2a
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-08 18:36:55 +1300

    add row clear

--- tetris.js
+++ tetris.js
@@ -21,6 +21,9 @@ function game(){
     let interval;
     let time = 500;
     let downEventFired = false;
+    let repaint = false;
+    let ignoreRow;
+    let dropCount = 0;
     window.addEventListener('keydown', event =>{
         function horizMove() {
             let prevMove = line.slice();
@@ -117,12 +120,44 @@ function game(){
                     for(let l = 0; l<4; l++){
                         takenTiles.push(shapeHis[l]);
                     }
+                    takenTiles = MergeSort(takenTiles);
+                    //for(let q = 10; q<=200; q+=10){
+                        let n = (takenTiles.length)-1;
+                        if(n>=9){
+                            for(let a = 0; a<=n;a++){
+                                if(a>=9 && (takenTiles[a] % 10) == 0 && takenTiles[a-9] == (takenTiles[a] - 9)){
+                                    repaint = true;
+                                    ignoreRow = takenTiles[a];
+                                    takenTiles.splice((a-9), 10)
+                                    n = (takenTiles.length)-1;
+                                    a-=10;
+                                    dropCount+=10;
+                                }
+                            }
+                            if(repaint == true){
+                                repaint = false;
+                                for(let r = 1; r<=200; r++){
+                                    document.getElementById(r).style.backgroundColor = 'rgb(90, 90, 240)';
+                                }
+                                for(let f = 0; f<takenTiles.length; f++){
+                                        if(takenTiles[f] <= (ignoreRow+1)){
+                                            takenTiles[f] += dropCount;
+                                        }
+                                    }
+                                    for(let w = 0; w<takenTiles.length; w++){
+                                        document.getElementById(takenTiles[w]).style.backgroundColor = 'red';
+                                    }
+                            }
+                        }
+                    //}
+                    console.log(takenTiles);
                     line = [5, 15, 25, 35];
+                    dropCount = 0;
+                    ignoreRow = undefined;
                     i = 0;
                     settled = false;
                     return;
                 }
-                    
                     document.getElementById(line[j]).style.backgroundColor = 'red';
                 }
                 
@@ -138,3 +173,53 @@ function game(){
     }
 }
 game();
+
+function MergeSort(A){
+    let n = A.length;
+    let Separate = function(A, p, r){
+        if(p==r){
+            return;
+        }
+        let q = Math.floor((p+r)/2);
+        Separate(A, p, q);
+        Separate(A, (q+1), r);
+        Merge(A, p, q, r);
+    }
+    let Merge = function(A, p, q, r){
+        let nl = q-p+1;
+        let nr = r-q;
+        let L = new Array(nl);
+        let R = new Array(nr);
+        for(let i = 0; i<=nl-1;i++){
+            L[i] = A[p+i];
+        }
+        for(let j = 0; j<=nr-1;j++){
+            R[j] = A[q+j+1];
+        }
+        let i=0;
+        let j=0;
+        let k=p;
+        while(i<nl&&j<nr){
+            if(L[i] <= R[j]){
+                A[k] = L[i];
+                i++;
+            }else{
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<nl){
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j<nr){
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    Separate(A, 0, n-1);
+    return(A)
+}
